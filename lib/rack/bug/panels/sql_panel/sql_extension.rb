e3968107404dba69712010c464f04bbacf953dfa
Ryan Sonnek <ryan@socialcast.com>
2011-08-12 09:37:22 -0500

    add rails 3.1 support

--- lib/rack/bug/panels/sql_panel/sql_extension.rb
+++ lib/rack/bug/panels/sql_panel/sql_extension.rb
@@ -1,8 +1,9 @@
 if defined?(ActiveRecord) &&  defined?(ActiveRecord::ConnectionAdapters)
   ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-    def log_with_rack_bug(sql, name, &block)
+    def log_with_rack_bug(*args, &block)
+      sql = args.first
       Rack::Bug::SQLPanel.record(sql, Kernel.caller) do
-        log_without_rack_bug(sql, name, &block)
+        log_without_rack_bug(*args, &block)
       end
     end
 
